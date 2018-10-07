55186e0c812454f00a91c79338b9830bf8c4bdc8
ymoustafa96 <m3ypm@live.com>
2018-10-07 18:55:24 -0400

    Update Containers, Actions, and Store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
+    compose (
+      applyMiddleware(ReduxPromise),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
   );
 
   if (module.hot) {
