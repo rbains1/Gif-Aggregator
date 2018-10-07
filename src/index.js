99936b718c8e57b3c2aa8783a69bce44704006ea
ymoustafa96 <m3ypm@live.com>
2018-10-07 16:33:15 -0400

    Addition of GifList and GifItem

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GifList from './components/GifList';
 import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
-  handleTermChange(term) {
-    console.log(term);
-  }
+    constructor() {
+        super();
 
-  render() {
-    return (
-      <div>
-        <SearchBar onTermChange={this.handleTermChange} />
-      </div>
-    );
-  }
+        this.state = {
+            gifs: [
+                {
+                    id: 1,
+                    url: 'http://fakeimg.pl/300/'
+                },
+                {
+                    id: 2,
+                    url: 'http://fakeimg.pl/300/'
+                },
+                {
+                    id: 3,
+                    url: 'http://fakeimg.pl/300/'
+                }
+            ]
+        }
+    }
+
+    handleTermChange(term) {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar onTermChange={this.handleTermChange} />
+                <GifList gifs={this.state.gifs} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
