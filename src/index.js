fb12e22df3ddef8b01c731042d1042d02be800dc
ymoustafa96 <m3ypm@live.com>
2018-10-07 18:18:39 -0400

    Addition of Modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GifList from './components/GifList';
+import GifModal from './components/GifModal';
 import SearchBar from './components/SearchBar';
 import request from 'superagent';
 import './styles/app.css';
@@ -10,10 +11,26 @@ class App extends React.Component {
         super();
 
         this.state = {
-            gifs: []
+            gifs: [],
+            selectedGif: null,
+            modalIsOpen: false
         };
+        
+        //this.handleTermChange = this.handleTermChange.bind(this);
+    }
 
-        this.handleTermChange = this.handleTermChange.bind(this);
+    openModal(gif) {
+        this.setState({
+            modalIsOpen: true,
+            selectedGif: gif
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            modalIsOpen: false,
+            selectedGif: null
+        });
     }
 
     handleTermChange(term) {
@@ -27,8 +44,10 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <SearchBar onTermChange={term => this.handleTermChange(term)}/>
-                <GifList gifs={this.state.gifs}/>
+                <SearchBar onTermChange={term => this.handleTermChange(term)} />
+                <GifList  gifs={this.state.gifs} onGifSelect={selectedGif => this.openModal(selectedGif) } />
+                <GifModal modalIsOpen={this.state.modalIsOpen} selectedGif={this.state.selectedGif}
+                          onRequestClose={ () => this.closeModal() } />
             </div>
         );
     }
