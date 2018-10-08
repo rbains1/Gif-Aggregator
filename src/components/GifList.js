b41ca2bc977af74d633f7a82546f8fb7bf50ccd9
ymoustafa96 <m3ypm@live.com>
2018-10-07 22:17:25 -0400

    Implemented Favorites Feature

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -2,13 +2,18 @@ import React from 'react';
 import GifItem from './GifItem';
 
 const GifList = (props) => {
-    const gifItems = props.gifs.map((image) => {
-        return <GifItem key={image.id} gif={image} onGifSelect={props.onGifSelect} />
-    });
+  const gifItems = props.gifs.map((image) => {
+    return <GifItem key={image.id}
+                    onGifSelect={props.onGifSelect}
+                    onFavoriteSelect={props.onFavoriteSelect}
+                    onFavoriteDeselect={props.onFavoriteDeselect}
+                    isAuthenticated={props.isAuthenticated}
+                    isFavorite={props.isFavorite} />
+  });
 
-    return (
-        <div className="gif-list">{gifItems}</div>
-    );
+  return (
+    <div className="gif-list">{gifItems}</div>
+  );
 };
 
 export default GifList;
